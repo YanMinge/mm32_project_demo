9c41bf563c28f805ee0a84a1642ad6a64a694334
Yanminge <guojia198769@163.com>
2021-11-18 13:20:54 +0800

    delete the bin copy in compile.sh

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ mkdir build
 cd build
 cmake -DMCU_TYPE=MM32SPIN0x ..
 make
-rm -rf ~/shardir/mm32_firmware/ai_set_motor.hex
-cp ai_set_motor.hex ~/shardir/mm32_firmware/
+#rm -rf ~/shardir/mm32_firmware/mm32_project_demo.hex
+#cp mm32_project_demo.hex ~/shardir/mm32_firmware/
